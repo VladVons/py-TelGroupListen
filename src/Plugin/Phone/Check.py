831d5557bb7994ba2259bc5fac1d8e19d343dfc5
VladVons <vladvons@gmail.com>
2025-03-25 06:31:46 +0200

    MyCommit

--- src/Plugin/Phone/Check.py
+++ src/Plugin/Phone/Check.py
@@ -4,10 +4,27 @@
 
 
 import logging
+import asyncio
+from telethon.tl.types import InputPhoneContact
+from telethon.tl.functions.contacts import ImportContactsRequest, DeleteContactsRequest
 from IncP.Plugin import TPlugin
 
 
 class TPCheck(TPlugin):
     async def Exec(self):
         for xPhone in self.Conf['phones']:
-            print(xPhone)
+            Contact = InputPhoneContact(
+                client_id = 0,
+                phone = xPhone,
+                first_name = f'tmp {xPhone}',
+                last_name = ''
+            )
+            Contacts = await self.Client(ImportContactsRequest([Contact]))
+            await asyncio.sleep(self.Conf['sleep'])
+            if (Contacts.users):
+                DCR = await self.Client(DeleteContactsRequest(id=[Contacts.users[0].id]))
+                User = DCR.users[0]
+                await asyncio.sleep(self.Conf['sleep'])
+                logging.info('phone %s is in telegram. firstname %s', xPhone, User.first_name)
+            else:
+                logging.info('phone %s is not in telegram', xPhone)
